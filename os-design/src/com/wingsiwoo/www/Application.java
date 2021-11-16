841793393b3eff44d32ae479a40fba59a26602b1
WingsiWoo <wingsiwoo@outlook.com>
2021-11-16 14:54:13 +0800

    操作系统课设

---
+++ os-design/src/com/wingsiwoo/www/Application.java
@@ -0,0 +1,12 @@
+package com.wingsiwoo.www;
+
+/**
+ * @author WingsiWoo
+ * @date 2021/11/15
+ */
+public class Application {
+    public static void main(String[] args) {
+        Scheduler scheduler = new Scheduler();
+        scheduler.start();
+    }
+}
